33d5ae6c43777d8288ff45726966353a5e55500b
Air2air <todd@dunningmarketing.com>
2021-07-19 23:29:10 -0700

    Updates

---
+++ precache-manifest.4484d2fafd8f6523dd4fd21fcfca1ed7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cf2370ffd74894bdfb9a54eac10608",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7141eea8a02affc4ae91",
+    "url": "./static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "b089a62a29bde685b0be",
+    "url": "./static/css/main.c500f3d5.chunk.css"
+  },
+  {
+    "revision": "7141eea8a02affc4ae91",
+    "url": "./static/js/2.ec187019.chunk.js"
+  },
+  {
+    "revision": "2494c22e0d61e29f4b75996640e9de43",
+    "url": "./static/js/2.ec187019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b089a62a29bde685b0be",
+    "url": "./static/js/main.58579d23.chunk.js"
+  },
+  {
+    "revision": "c7d1e820a23ce34cbe1f",
+    "url": "./static/js/runtime-main.97c3210d.js"
+  },
+  {
+    "revision": "6751a581b5f362d8adf18f3e044055ab",
+    "url": "./static/media/logo.6751a581.svg"
+  }
+]);
--- src/pages/Home/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <>Your browser does not support the Canvas element.</>;
-};
-
-export default Home;
